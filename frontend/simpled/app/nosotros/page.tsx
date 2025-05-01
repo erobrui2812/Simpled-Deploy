30a9623814184ff80852df2a0a7075535868f3fa
Elías Robles Ruiz <149778129+erobrui2812@users.noreply.github.com>
2025-05-01 18:22:29 +0200

    Arreglar banner minusculas

--- frontend/simpled/app/nosotros/page.tsx
+++ frontend/simpled/app/nosotros/page.tsx
@@ -1,4 +1,4 @@
-import Banner from '@/components/ABanner';
+import Banner from '@/components/Banner';
 import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent } from '@/components/ui/card';
